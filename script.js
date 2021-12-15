118e7c5b41bd9b90c567d8dc64bde8f969ec2db2
renandcr <renandamagazine@gmail.com>
2021-12-15 17:44:34 -0300

    Enviar atividade acabada.

---
+++ script.js
@@ -0,0 +1,85 @@
+// Sabor Brasileiro
+// Bairro das Laranjeiras
+// Dez Anos
+// Comida típica Nordestina
+// Segunda à Sabado
+// 12:00 até 21:00
+
+class Restaurante{
+    constructor(nome, endereço, experiência, expecialidade, dias, horário){
+        this.nome = "Sabor Brasileiro"
+        this.endereço = "Bairro das Laranjeiras"
+        this.experiência = "Dez anos"
+        this.especialidade = "Comida típica Nordestina"
+        this.dias = "Segunda à sábado"
+        this.horário = "12:00 até 21:00"
+
+    }
+
+        fazerPedido(){
+
+        }
+
+        pagarConta(){
+
+        }
+
+        darFeedback(){
+
+        }
+    
+}
+
+// Burguer Queen
+// fast food
+// vinte anos
+// hamburgueres
+// centro
+// todos os dias da semana
+// das 9:00 até às 22:00
+
+// fazer pedido
+// pagar
+// crítica
+
+
+class Restaurante{
+    constructor(nome, segmento, experiência, especialidade, endereço, dias, horário){
+        this.nome = "Burguer Queen"
+        this.segmento = "Fast Food"
+        this.experiência = "Vinte anos"
+        this.especialidade = "Hamburgueres"
+        this.endereço = "Centro"
+        this.dias = "Todos os dias da semana"
+        this.horário = "Das 09:00 até às 22:00"
+    }
+
+    fazerPedido(){
+
+    }
+
+    pagarConta(){
+
+    }
+
+    darFeedback(){
+
+    }
+}
+
+
+// Corsa
+// 2012
+// gasolina/etanol
+// seminovo
+// econômico
+
+class Carro{
+    constructor(modelo, ano, combustível, conservação, consumo){
+        this.modelo = "Corsa"
+        this.ano = "2012"
+        this.combustível = "Gasolina/Etanol"
+        this.conservação = "Seminovo"
+        this.consumo = "Econômico"
+    }
+}
